299cc058f8c843d223d8b12557e6f9f1760d1f7a
JoseIsaac6858 <70914406+JoseIsaac6858@users.noreply.github.com>
2020-09-07 13:44:46 -0500

    Add files via upload

---
+++ index.html/scripts/js.js
@@ -0,0 +1,9 @@
+var name = prompt("Ingreasa tu nombre");
+var edad = prompt("Ingreasa tu edad");
+var nacimiento = prompt("Ingreasa tu fecha de nacimiento");
+var correo = prompt("Ingreasa tu correo electronico");
+var numero = prompt("Ingreasa tu numero telefonico");
+
+alert("tus datos <br>" + " " + name + ", " + edad + ", " + nacimiento + ", " + correo + ", " + numero + ".");
+name = 15 
+alert("bienvendio");
